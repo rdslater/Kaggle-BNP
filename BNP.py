34a14b0cbe6181f59b1485f22f71c07d0603be78
rdslater <rdslater@gmail.com>
2016-02-23 18:48:06 -0600

    Added files via upload
    Cleanup on initial ipython notebook--inputs were out of order, lots of experimental code.

--- BNP.py
+++ BNP.py
@@ -1,32 +1,4 @@
 
-# coding: utf-8
-
-# In[1]:
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-get_ipython().magic('matplotlib inline')
-
-data = pd.read_csv("C://Users//Robert//Downloads//train.csv~//train.csv")
-
-result=data.iloc[:,1].as_matrix
-v1_0.head(10)
-x=v1_0.v1
-y=v1_0.v2
-col=v1_0.target
-plt.scatter(x,y,c=col)
-
-
-# In[26]:
-
-v1_0=data.iloc[:,1:4].dropna()
-v1_1=data["v1"][data.target==1].dropna()
-
-
-# In[13]:
-
-
 # Imports
 
 # pandas
@@ -38,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('whitegrid')
-get_ipython().magic('matplotlib inline')
+%matplotlib inline
 
 # machine learning
 from sklearn.linear_model import LogisticRegression
@@ -49,18 +21,12 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn import cross_validation
 import xgboost as xgb
 
-
-# In[14]:
-
 # get bnp & test csv files as a DataFrame
 bnp_df   = pd.read_csv("C://Users//Robert//Downloads//train.csv~//train.csv")
 test_df  = pd.read_csv("C://Users//Robert//Downloads//test.csv~//test.csv")
 
 # preview the data
 bnp_df.head()
-
-
-# In[15]:
 
 for f in bnp_df.columns:
     # fill NaN values with mean
@@ -72,16 +38,8 @@ for f in bnp_df.columns:
     elif bnp_df[f].dtype == 'object':
         bnp_df[f][bnp_df[f] != bnp_df[f]] = bnp_df[f].value_counts().index[0]
         test_df[f][test_df[f] != test_df[f]] = test_df[f].value_counts().index[0]
-
-
-# In[19]:
-
-bnp_df.head()
-
-
-# In[16]:
-
-from sklearn import preprocessing
+		
+		from sklearn import preprocessing
 
 for f in bnp_df.columns:
     if bnp_df[f].dtype == 'object':
@@ -89,51 +47,7 @@ for f in bnp_df.columns:
         lbl.fit(np.unique(list(bnp_df[f].values)  + list(test_df[f].values)))
         bnp_df[f]   = lbl.transform(list(bnp_df[f].values))
         test_df[f]  = lbl.transform(list(test_df[f].values))
-
-
-# In[10]:
-
-bnp_df.head()
-
-
-# In[17]:
-
-X_train = bnp_df.drop(["ID","target"],axis=1)
-Y_train = bnp_df["target"]
-X_test  = test_df.drop("ID",axis=1).copy()
-
-
-# In[24]:
-
-
-# Random Forests
-from sklearn.ensemble import RandomForestClassifier
-random_forest = RandomForestClassifier(n_estimators=100)
-
-random_forest.fit(X_train, Y_train)
-
-Y_pred = random_forest.predict_proba(X_test)
-
-
-
-# In[11]:
-
-
-Y_pred = random_forest.predict_proba(X_test)
-
-
-# In[23]:
-
-
-# Logistic Regression
-
-logreg = LogisticRegression()
-
-logreg.fit(X_train, Y_train)
-
-Y_pred = logreg.predict_proba(X_test)[:,1]
-
-logreg.score(X_train, Y_train)
+		
 # get Coefficient of Determination(R^2) for each feature using Logistic Regression
 coeff_df = DataFrame(bnp_df.columns.delete([0,1]))
 coeff_df.columns = ['Features']
@@ -147,73 +61,35 @@ coeff_ser = Series(list(coeff_df["Coefficient Estimate"]), index=coeff_df["Featu
 fig = coeff_ser.plot(kind='barh', figsize=(20,5))
 fig.set(ylim=(100, 131))
 
-
-# In[24]:
-
-Y_pred
-
-
-# In[62]:
-
+#look at most significant param
 x=bnp_df.loc[:,'v50'][bnp_df['target']==0]
 y=bnp_df.loc[:,'v50'][bnp_df['target']==1]
-
-
-# In[77]:
-
 data=[x,y]
 plt.boxplot(data,0, 'D',labels=(0,1))
-
 plt.show()
+# not much to see
+		
 
-
-# In[70]:
-
-help(plt.boxplot)
-
-
-# In[27]:
-
-coeff_ser = Series(list(coeff_df["Coefficient Estimate"]), index=coeff_df["Features"]).sort_values()
-fig = coeff_ser.plot(kind='barh', figsize=(20,5))
-fig.set(ylim=(100, 131))
-fig.set(xlim=(0,0.01))
-
-
-# In[29]:
-
-Y_pred[:,1]
-
-
-# In[40]:
-
-# Create submission
-
-submission = pd.DataFrame()
-submission["ID"]            = test_df["ID"]
-submission["PredictedProb"] = Y_pred[:,1]
-
-submission.to_csv('bnp.csv', index=False)
-
-
-# In[39]:
-
-tt=X_test.iloc[:,(49,65,11,30,128,71,9,61,37,46,113,23,109,13,39)]
-tst=X_train.iloc[:,(49,65,11,30,128,71,9,61,37,46,113,23,109,13,39)]
+#random forest attempt all data
+X_train = bnp_df.drop(["ID","target"],axis=1)
+Y_train = bnp_df["target"]
+X_test  = test_df.drop("ID",axis=1).copy()
+# Random Forests
 from sklearn.ensemble import RandomForestClassifier
 random_forest = RandomForestClassifier(n_estimators=100)
-
 random_forest.fit(X_train, Y_train)
-
 Y_pred = random_forest.predict_proba(X_test)
 
+#further visualization
+#coeff_ser = Series(list(coeff_df["Coefficient Estimate"]), index=coeff_df["Features"]).sort_values()
+#fig = coeff_ser.plot(kind='barh', figsize=(20,5))
+#fig.set(ylim=(100, 131))
+#fig.set(xlim=(0,0.01))
 
-# In[41]:
-
-Y_pred
-
-
-# In[ ]:
-
-
-
+#random forest based on top significant columns
+#tt=X_test.iloc[:,(49,65,11,30,128,71,9,61,37,46,113,23,109,13,39)]
+#tst=X_train.iloc[:,(49,65,11,30,128,71,9,61,37,46,113,23,109,13,39)]
+#from sklearn.ensemble import RandomForestClassifier
+#random_forest = RandomForestClassifier(n_estimators=100)
+#random_forest.fit(tst, Y_train)
+#Y_pred = random_forest.predict_proba(tt)
